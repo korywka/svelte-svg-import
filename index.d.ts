754a30263d6e8d33df93728770931b52dcf58dcc
korywka <neporozhniy@gmail.com>
2023-08-22 21:24:37 +0200

    fix ts support

--- index.d.ts
+++ index.d.ts
@@ -1,14 +1,4 @@
-declare module 'svelte-svg-import' {
-	import type { Plugin } from 'vite'
-	export default function plugin(): Plugin
-}
-
-declare module '*.svg?svelte' {
-	import type { ComponentType, SvelteComponent } from 'svelte'
-	import type { SVGAttributes } from 'svelte/elements'
-
-	type Constraint = SVGAttributes<SVGSVGElement>
-	const content: ComponentType<SvelteComponent<Constraint>>
-
-	export default content
-}
+/**
+ * @returns {import('vite').Plugin}
+ */
+export default function plugin(): import('vite').Plugin;
